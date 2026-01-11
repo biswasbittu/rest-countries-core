fbf1d6a10753fc9b924fa124b2b5489283e23f8b
Bittu Biswas <biswasbittu7415@yahoo.com>
2026-01-11 12:04:52 +0530

    staste done

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Country = ({ countrys }) => {
     // console.log(countrys)
     const { country, countryInfo, continent, population } = countrys;
-
-
+    const [visited, setVisited] = useState(false)
+    const handleVisited = () => {
+        // console.log('visited btn clicked')
+        setVisited(!visited)
+        
+    }
     return (
         <div className=' mt-16'>
             <div className="card bg-base-100 w-96 shadow-sm">
@@ -20,10 +24,19 @@ const Country = ({ countrys }) => {
                     </div>
                     <p>{population}</p>
                     <div className="card-actions justify-start">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button onClick={() => handleVisited()}
+                            className={`btn ${visited ? "btn- btn-secondary" : 'btn-primary'}`}>{visited ? "Visited" : 'Not Visited'}</button>
                     </div>
                 </div>
             </div>
+            {visited && (
+                <div role="alert" className="alert alert-info">
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="h-6 w-6 shrink-0 stroke-current">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                    </svg> */}
+                    <span>Visited {country}</span>
+                </div>
+            )}
         </div>
     );
 };
